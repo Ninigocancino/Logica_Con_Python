69f81f8c19421d1ee942d9b61828696fc03d97d9
VictorCancino <juanvictorcancino@gmail.com>
2025-01-30 11:43:30 -0600

    Se completó el ejercicio 002

---
+++ Fundamentos_Basicos/Media/Ejercicio_002.py
@@ -0,0 +1,7 @@
+
+#Enunciado: "A la variable llamada radio, se le asigna el valor 5. La variable llamada 'pi' tiene asignado el valor  3.1416. La variable 'area' guarda una formula que toma la variable 'pi' y la multiplica por la variable 'radio' que esta elevada al cuadrado. Finalmente, se imprime en la consola el valor del área."
+
+radio = 5
+pi = 3.1416
+area = pi * radio ** 2
+print(area)
