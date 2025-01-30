24849c4e9cb93c29b81f19d8d1dee2846a8c552a
VictorCancino <juanvictorcancino@gmail.com>
2025-01-29 22:10:23 -0600

    Se completó el ejercicio 004

---
+++ Ejercicio_004.py
@@ -0,0 +1,11 @@
+
+#Enunciado: "Si el número es mayor que 0, entonces imprime 'Positivo'. Si el número es menor que 0, entonces imprime 'Negativo'. Si el número es igual a 0, entonces imprime 'Cero'."
+
+numero = 0
+
+if numero > 0:
+    print("Positivo")
+elif numero < 0:
+    print('Negativo')
+elif numero == 0:
+    print('Cero')
