e3d9890d47d1ea6b5019db77c83f418bef9ebb0f
VictorCancino <juanvictorcancino@gmail.com>
2025-01-29 21:42:01 -0600

    Se completó el ejerccio 002

---
+++ Ejercicio_002.py
@@ -0,0 +1,9 @@
+
+# Enunciado: "Si la edad es mayor o igual a 18, imprime 'Eres mayor de edad'. De lo contrario, imprime 'Eres menor de edad'."
+
+edad = 17
+
+if edad >= 18:
+    print("Eres mayor de edad")
+else:
+    print("Eres menor de edad")
