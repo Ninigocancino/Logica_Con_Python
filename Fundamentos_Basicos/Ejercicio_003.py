e80ba16486e20e743da655ac4a6b4030e33efa7b
VictorCancino <juanvictorcancino@gmail.com>
2025-01-29 23:09:56 -0600

    Se completó el ejercicio 003

---
+++ Fundamentos_Basicos/Ejercicio_003.py
@@ -0,0 +1,5 @@
+
+#Enunciado: "La variable llamada mensaje y tiene el valor 'Bienvenido al curso de Python'. Luego, se imprime en la consola el valor de la variable mensaje."
+
+mensaje = 'Bienvenido al curso e Python'
+print(mensaje)
