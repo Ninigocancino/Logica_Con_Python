4e348b45c48921e1ea3673571b99544262a43641
VictorCancino <juanvictorcancino@gmail.com>
2025-01-29 23:49:39 -0600

    Se completó el Ejercicio 001.py del directorio 'Media'

---
+++ Fundamentos_Basicos/Media/Ejercicio_001.py
@@ -0,0 +1,5 @@
+
+#Enunciado: "Se solicita al usuario que ingrese su nombre. El nombre se guarda en la variable 'nombre'. Luego, se imprime en la consola el mensaje 'Hola, [nombre]', donde [nombre] es el valor ingresado por el usuario."
+
+nombre = input("Ingresa tu nombre: ")
+print("Hola " + nombre)
