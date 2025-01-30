95944f3dd79811e741ae9166817bb3c390e608fd
VictorCancino <juanvictorcancino@gmail.com>
2025-01-30 12:02:45 -0600

    Se completó el ejercicio 0003

---
+++ Fundamentos_Basicos/Media/Ejercicio_003.py
@@ -0,0 +1,7 @@
+
+#Enunciado: "Se pide al usuario que ingrese un valor numero que se combierte de tipo string a tipo numerico y se almacenará en la variable 'lado_1'. Luego se declara una variable llamada 'lado_2' que tiene asignado el valor 5. Luego se declara una variable 'operacion' que guarda una formula que toma 'lado_1' y lo multiplica por  'lado_2'. Finalmente se imprime la variable 'operacion'"
+
+lado_1 = int(input("Ingresa un valor numerico: "))
+lado_2 = 5
+operacion = lado_1 * lado_2
+print(operacion)
