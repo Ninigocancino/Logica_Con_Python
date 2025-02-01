f739c94c5124ae6a1abd4574e6274832ca7de754
VictorCancino <juanvictorcancino@gmail.com>
2025-01-31 22:55:15 -0600

    Se completó el ejercicio 001 de dificultad facil en el directorio '02_Estructuras_de_Control'

---
+++ 02_Estructuras_de_Control/01_Condicionales/Ejerccio_001.py
@@ -0,0 +1,6 @@
+
+#Instrucción en lenguaje natural: En la variable 'edad' guarda el valor 18 como tipo entero. Luego  verifica si la variable 'edad' es mayor o igual a 18. Si la condición es verdadera, entonces imprime el mensaje 'Eres mayor de edad'
+
+edad = 18
+if edad >= 18:
+    print("Eres mayor de edad")
