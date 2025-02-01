7eef166c67830b80a0908b0970de5aadbdc467cf
VictorCancino <juanvictorcancino@gmail.com>
2025-02-01 17:14:04 -0600

    Se completó el ejercicio 002

---
+++ 02_Estructuras_de_Control/01_Condicionales/Dificultad_Media/Ejercicio_002.py
@@ -0,0 +1,9 @@
+
+#Instrucciones en lenguajes natural: Declara una variable llamada número a la que le asignes un valor numérico 7, luego verifica si la variable numero es divisible entre dos y su residuo es igual 0. Si la condición se cumple, entonces imprime en consola el mensaje "El número es par". Pero si la condición es  falsa entonces imprime el mensaje "El número es impar"
+
+
+numero = 7
+if numero % 2 == 0:
+    print("El número es par")
+else:
+    print("El número es impar")
