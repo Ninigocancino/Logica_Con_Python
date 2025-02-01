019678a3f024f61b5ba247442a66383da3222ebb
VictorCancino <juanvictorcancino@gmail.com>
2025-01-31 23:18:20 -0600

    Se completó el ejercicio

---
+++ 02_Estructuras_de_Control/01_Condicionales/Ejercicio_003.py
@@ -0,0 +1,8 @@
+
+#Instrucciones en lenguaje natural: Declara una variable 'puntaje' y guarda en dicha variable el número 85. Luego verifica si la variable 'puntaje' es mayor o igual que 70, si la condición es verdadera, entonces imprime en consola el mensaje 'Aprobado'
+
+#Instrucciones en código Python:
+
+puntaje = 85
+if puntaje >= 70:
+    print("Aprobado")
