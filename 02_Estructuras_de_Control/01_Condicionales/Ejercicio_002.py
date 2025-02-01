b85130b90e168e3091e267abd5e0f0c5149a909a
VictorCancino <juanvictorcancino@gmail.com>
2025-01-31 23:08:57 -0600

    Se completó el ejercicio'

---
+++ 02_Estructuras_de_Control/01_Condicionales/Ejercicio_002.py
@@ -0,0 +1,8 @@
+
+#Instrucción en lenguaje natural: Declara una variable 'numero', en dicha variable guarda el valor -5 que es de tipo entero negativo. Luego verifica si la variable 'numero' es menor que cero, si la condición es verdadera, entonces imprime en consola el mensaje 'El número es negativo'
+
+#Traducción a lenguaje Python:
+
+numero = -5
+if numero < 0:
+    print('El numero es negativo')
