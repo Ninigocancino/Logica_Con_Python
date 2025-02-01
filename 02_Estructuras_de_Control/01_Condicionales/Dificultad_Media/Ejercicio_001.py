8d163efb5236395918ab4d1e3499e479ab571cca
VictorCancino <juanvictorcancino@gmail.com>
2025-01-31 23:32:23 -0600

    Se completó el ejercicio

---
+++ 02_Estructuras_de_Control/01_Condicionales/Dificultad_Media/Ejercicio_001.py
@@ -0,0 +1,9 @@
+
+#Instrucciones en lenguaje natural: Declara una variable llamada 'edad', guarda en dicha variable el valor 20. Luego verifica si la variable 'edad' es mayor o igual que 18. Si la condición es verdadera, entonces imprime en consola el mensaje 'Eres mayor de edad'. Pero si la condición es falsa, entonces imprime en consola el mensaje 'Eres menor de edad.
+
+#Instrucciones en Python:
+edad = 20
+if edad >= 18:
+    print('Eres mayor de edad')
+else:
+    print('Eres menor de edad')
