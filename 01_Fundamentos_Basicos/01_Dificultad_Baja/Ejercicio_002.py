9cc6528cef7c13362aa308ee9663b91677373e03
VictorCancino <juanvictorcancino@gmail.com>
2025-02-04 09:06:59 -0600

    Se modificó la documentación del ejercicio 002

--- 01_Fundamentos_Basicos/01_Dificultad_Baja/Ejercicio_002.py
+++ 01_Fundamentos_Basicos/01_Dificultad_Baja/Ejercicio_002.py
@@ -1,5 +1,5 @@
 
-#Enunciado: la variable nombre tiene el valor 'Felipe', luego la variable nombre se imprime en consola
+#Instucción en lenguaje natural: Declara una variable llamada 'nombre', luego asignale el valor 'Felipe' de tipo string, luego imprime la variable nombre en consola
 
-nombre = 'Felipe'
-print(nombre)
+nombre = 'Felipe' #Se declara la variable nombre y se le asigna un valor de tipo string
+print(nombre) #Se imprime o muestra en consola el contenido o el valor almacenado en la variable 'nombre'
