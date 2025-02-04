15e46eff210778996fabf5d5f9fa8f264ae8bd36
VictorCancino <juanvictorcancino@gmail.com>
2025-02-04 16:14:16 -0600

    Se completó el ejercicio

---
+++ 01_Fundamentos_Basicos/02_Dificultad_Media/Ejercicio_009.py
@@ -0,0 +1,6 @@
+
+#Instrucciones en lenguaje natural: Pide al usuario que ingrese su altura en metros, luego guarda la respeusta del usuario en una variable llamada'altura'. Después imprime la variable 'altura'.
+
+
+altura = float(input("Ingresa tu altura: "))  #Se pide al usuario que ingrese su altura
+print(altura) #Imprimeen pantalla el valor de la variable 'altura'
