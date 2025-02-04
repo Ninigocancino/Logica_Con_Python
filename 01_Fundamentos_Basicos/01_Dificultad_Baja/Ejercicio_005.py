75c5f643c0ee3ff7995045ce55060453fb226a73
VictorCancino <juanvictorcancino@gmail.com>
2025-02-04 09:42:04 -0600

    Se completó el ejercicio 005

---
+++ 01_Fundamentos_Basicos/01_Dificultad_Baja/Ejercicio_005.py
@@ -0,0 +1,5 @@
+
+#Instrucciones en lenguaje natural: Declará una variable 'operacion' y guarda en ella la suma de 1 y 3. Luego imprime la varaibale 'operacion'
+
+operacion = 1 + 3
+print(operacion)
