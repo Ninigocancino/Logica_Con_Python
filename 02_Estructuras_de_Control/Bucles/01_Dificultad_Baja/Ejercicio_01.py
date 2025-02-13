9643672edc92a3bd003758d45a37bd945954e443
VictorCancino <juanvictorcancino@gmail.com>
2025-02-12 20:12:24 -0600

    Se completó el ejercicio 01 en el directorio 'Dificultad baja' en el tema 'Bucles'

---
+++ 02_Estructuras_de_Control/Bucles/01_Dificultad_Baja/Ejercicio_01.py
@@ -0,0 +1,5 @@
+
+#Inicia un bucle que itere en una variable 'i' sobre una secuencia de números generada por range(5), la cual incluye los valores del 0 al 4. En cada iteración, se asigna el valor actual a la variable i y se imprime en la consola el valor de i.
+
+for i in range(5):
+    print(i)
