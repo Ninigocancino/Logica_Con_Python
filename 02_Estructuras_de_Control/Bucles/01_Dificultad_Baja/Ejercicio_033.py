e23e125e2fd34fda55f70bb3695e738bad71a4c7
VictorCancino <juanvictorcancino@gmail.com>
2025-02-13 10:12:09 -0600

    Se completó el ejercicio 033

---
+++ 02_Estructuras_de_Control/Bucles/01_Dificultad_Baja/Ejercicio_033.py
@@ -0,0 +1,5 @@
+
+#Inicia un ciclo donde el iterador 'i' inicie una secuencia en un rango que va desde el número 0 y llegue hasta el número 10 con saltos de 2 en dos. Luego imprime el valor que toma 'i' en cada ciclo o iteración
+
+for i in range(0, 10,2):
+    print(i)
