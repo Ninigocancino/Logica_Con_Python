2fb8906d4a6041584a0cbeb04d18920abf36d7e0
VictorCancino <juanvictorcancino@gmail.com>
2025-02-13 09:46:51 -0600

    Se completo el ejercicio 02

---
+++ 02_Estructuras_de_Control/Bucles/01_Dificultad_Baja/Ejercicio_02.py
@@ -0,0 +1,5 @@
+# Inicia un ciclo donde el iterador 'i' inicie una secuencia en un rango que va desde el número 1 hasta el 5. Imprime el valor que toma el iterador 'i' en cada iteración.
+
+for i in range(1,6):
+    print(i)
+
