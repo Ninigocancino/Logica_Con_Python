a9ad9951bb9a2f191cdc3e59dd3b6bbadc2e2090
VictorCancino <juanvictorcancino@gmail.com>
2025-02-15 10:07:33 -0600

    Se corrigió la instrucción del ejercicio 037

--- 02_Estructuras_de_Control/Bucles/02_Difiultad_Media/Ejercicio_037.py
+++ 02_Estructuras_de_Control/Bucles/02_Difiultad_Media/Ejercicio_037.py
@@ -1,5 +1,5 @@
 
-#Declara una variable llamada 'numeros' y asígnale como valor una lista que contenga los numero '1,2,3,4,5'. Luego inicia un ciclo donde la variable numero es el iterador que recorre cada elemento 'numero' en la variable 'numeros', luego verifica si el residuo del iterador dividido entre 2 es igual a 0, entonces imprime el valor de 'numero' y concatena el mensaje 'es par' . Si la condición no se cumple entonces imprime el valor de 'numero' y concatena el mensaje 'es impar'
+#Instrucción en lenguaje natural: Declara una variable llamada 'numeros' y asígnale como valor una lista que contenga los numero '1,2,3,4,5'. Luego inicia un ciclo donde la variable numero sea el iterador que recorre cada elemento en la variable 'numeros', luego verifica en cada iteración si el residuo del iterador dividido entre 2 es igual a 0, si la condición se cumple entonces imprime el valor de 'numero' y concatena el mensaje 'es par'. Si la condición no se cumple entonces imprime el valor de 'numero' y concatena el mensaje 'es impar'
 
 numeros = [1,2,3,4,5]
 
