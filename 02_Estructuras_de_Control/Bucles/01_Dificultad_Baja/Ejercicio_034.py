10524b05cf24a83e020301034e7421727f300757
VictorCancino <juanvictorcancino@gmail.com>
2025-02-15 09:29:24 -0600

    Se completó el ejercicio 034

---
+++ 02_Estructuras_de_Control/Bucles/01_Dificultad_Baja/Ejercicio_034.py
@@ -0,0 +1,5 @@
+
+#Inicia un ciclo donde el iterador  ´í´ inicie un secuencia en un rango que va desde 5 hasta 0 con decrementos de 1. Imprime el valor que 'i' toma en cada ciclo
+
+for i in range(5,0,-1):
+    print(i)
