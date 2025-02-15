ae300ac6e026c8721cc8a1d4b77eea4c96ab3143
VictorCancino <juanvictorcancino@gmail.com>
2025-02-15 10:59:39 -0600

    Se completó el ejercicio 038

---
+++ 02_Estructuras_de_Control/Bucles/02_Difiultad_Media/Ejercicio_038.py
@@ -0,0 +1,6 @@
+#Instrucciones en lenguaje natural: Declara una variable llamada 'palabra' y asígnale el valor 'Python'. Luego inicia un ciclo donde el iterador sea una variable 'letra' y recorra cada elemento dentro de la variable 'palabra'. Por último imprime el valor del iterador en cada ciclo
+
+palabra = 'Python'
+
+for letra in  palabra:
+    print(letra)
