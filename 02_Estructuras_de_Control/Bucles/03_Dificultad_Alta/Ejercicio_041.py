6a87a6206dd415199cdf2216af9f16a2e83b7d7d
VictorCancino <juanvictorcancino@gmail.com>
2025-03-06 21:46:26 -0600

    Se agregó como un comentario la forma en que se lee el código del ejercicio

--- 02_Estructuras_de_Control/Bucles/03_Dificultad_Alta/Ejercicio_041.py
+++ 02_Estructuras_de_Control/Bucles/03_Dificultad_Alta/Ejercicio_041.py
@@ -1,4 +1,5 @@
-#Instrucciones en lenguaje natural: Inicia un ciclo donde el iterador 'i' tome valores en un rango de 1 a 5, a la vez inicia un ciclo interno donde el iterador 'j' tome valores en un rango de 1 a 5. Si la suma de 'i' + 'j' es igual a 6, entonces imprime el símbolo '*' y disponlo de forma horizontal. En caso contrario imprime el símbolo '-'. Por ultimo imprime una línea vacía en cada ciclo principal.
+#Así le daría las instrucicones a Python en lenguaje natural: 
+#Inicia un ciclo donde el iterador 'i' tome valores en un rango de 1 a 5, a la vez inicia un ciclo interno donde el iterador 'j' tome valores en un rango de 1 a 5. Si la suma de 'i' + 'j' es igual a 6, entonces imprime el símbolo '*' seguido de un espacio y disponlo de forma horizontal. En caso contrario imprime el símbolo '-' seguido de un espacio y disponlo de forma horizontal. Por ultimo imprime una línea vacía por cada ciclo principal terminado.
 
 
 for i in range(1, 6):
@@ -8,3 +9,7 @@ for i in range(1, 6):
         else:
             print("-", end=" ")
     print()
+
+
+#Así leería el código si tuviera que expresarlo en voz 
+#Por cada valor de i en el rango de 1 a 5, y por cada valor de j en el rango de 1 a 5, si la suma de i y j es igual a 6, imprime "*" seguido de un espacio. En caso contrario, imprime un espacio doble. Al final de cada ciclo interno (j), se imprime un salto de línea (print()).
