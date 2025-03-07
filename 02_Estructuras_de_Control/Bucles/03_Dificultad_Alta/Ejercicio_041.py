f3d6b0b2e6cffc9d6c93b7b49c6de3979be3f168
VictorCancino <juanvictorcancino@gmail.com>
2025-03-06 21:38:04 -0600

    Se cambió el espacio vacio por el símbolo '-' en el else del ejejercicio

--- 02_Estructuras_de_Control/Bucles/03_Dificultad_Alta/Ejercicio_041.py
+++ 02_Estructuras_de_Control/Bucles/03_Dificultad_Alta/Ejercicio_041.py
@@ -1,4 +1,4 @@
-#Instrucciones en lenguaje natural: Inicia un ciclo donde el iterador 'i' tome valores en un rango de 1 a 5, a la vez inicia un ciclo interno donde el iterador 'j' tome valores en un rango de 1 a 5. Si la suma de 'i' + 'j' es igual a 6, entonces imprime el símbolo '*' y disponlo de forma horizontal. En caso contrario imprime dos espacios vacíos. Por ultimo imprime una línea vacía en cada ciclo principal.
+#Instrucciones en lenguaje natural: Inicia un ciclo donde el iterador 'i' tome valores en un rango de 1 a 5, a la vez inicia un ciclo interno donde el iterador 'j' tome valores en un rango de 1 a 5. Si la suma de 'i' + 'j' es igual a 6, entonces imprime el símbolo '*' y disponlo de forma horizontal. En caso contrario imprime el símbolo '-'. Por ultimo imprime una línea vacía en cada ciclo principal.
 
 
 for i in range(1, 6):
@@ -6,5 +6,5 @@ for i in range(1, 6):
         if i + j == 6:
             print("*", end=" ")
         else:
-            print(" ", end=" ")
+            print("-", end=" ")
     print()
