f54110148101893c492b6af53acb65c5c35fadd1
VictorCancino <juanvictorcancino@gmail.com>
2025-03-12 19:23:33 -0600

    Se completó el ejercicio 46

---
+++ 02_Estructuras_de_Control/03_Listas/01_Dificultad_baja/Ejercicio_046.py
@@ -0,0 +1,5 @@
+# Crea una lista  que contenga los numeros del 1 al 5, guarda la lista en una variable llamada 'numeros'. Inicia un ciclo que recorra/itere cada elemento 'numero' dentro de la lista 'numeros', asignando el valor actual de cada iteración a la variable 'numero'. Después imprime cada elemento numero.
+
+numeros = [1,2,3,4,5]
+for numero in numeros:
+    print(numero)
