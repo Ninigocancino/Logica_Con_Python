f40a60883efd049d5aac8264929569c8939050db
VictorCancino <juanvictorcancino@gmail.com>
2025-03-12 20:48:19 -0600

    Se completó el ejercicio 50

---
+++ 02_Estructuras_de_Control/03_Listas/01_Dificultad_baja/Ejercicio_050.py
@@ -0,0 +1,6 @@
+# Crea una lista llamada numeros que guarde los elementos 10,20,30,40,50. Luego inicia un ciclo donde el iterador numero recorra los elementos de la lista numeros y tome el valor actual en cada iteracion. Después verifica si el iterador numero es mayor a 30 si la condición se cumple imprime consola el valor del iterador numero en cada iteración
+
+numeros = [10,20.30,40,50]
+for numero in numeros:
+    if numero > 30:
+        print(numero)
