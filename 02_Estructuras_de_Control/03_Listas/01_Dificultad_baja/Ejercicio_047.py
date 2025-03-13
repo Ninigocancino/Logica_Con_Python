bbb984819c198e344bbecf580263b6addcc10bd2
VictorCancino <juanvictorcancino@gmail.com>
2025-03-12 19:53:08 -0600

    Se completó el ejercicio 47

---
+++ 02_Estructuras_de_Control/03_Listas/01_Dificultad_baja/Ejercicio_047.py
@@ -0,0 +1,5 @@
+# Crea una lista llamada frutas que contenga los valores ['manzana', 'banana', 'cereza']. Luego, inicia un bucle que itera sobre cada elemento de la lista frutas con el iterador fruta, asigna el valor actual a la variable fruta. En cada iteración, imprime en la consola el mensaje 'Me gusta la' seguido del valor de fruta en cada ciclo."
+
+frutas = ['manzana', 'banana', 'cereza']
+for fruta in frutas:
+    print("Me gusta la", fruta)
