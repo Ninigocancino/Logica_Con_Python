5a271aac5f3274c541d9483a45353936d580b8b5
VictorCancino <juanvictorcancino@gmail.com>
2025-03-12 20:37:41 -0600

    Se completo el ejercicio 49

---
+++ 02_Estructuras_de_Control/03_Listas/01_Dificultad_baja/Ejercicio_049.py
@@ -0,0 +1,5 @@
+# Crea una lista llamada nombres que guarde los elemento 'Ana', 'Luis' y 'Carlos. Luego inicia un ciclo donde el iterador nombre recorra los elementos de la lista nombres y tome el valor actual de cada iteración. Luego imprime para cada ciclo el mensaje "Hola" y concatena el valor que toma el iterador nombre.
+
+nombres = ['Ana','Luis', 'Carlos']
+for nombre in nombres:
+    print("Hola",nombre)
