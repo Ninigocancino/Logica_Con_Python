733aadc273e5578eef0f52938fa2846960a8ff4e
VictorCancino <juanvictorcancino@gmail.com>
2025-03-12 20:23:11 -0600

    Se completó el ejercicio 48

---
+++ 02_Estructuras_de_Control/03_Listas/01_Dificultad_baja/Ejercicio_048.py
@@ -0,0 +1,5 @@
+# Crea una lista llamada pares que contiene los valores 2, 4, 6, 8, 10. Luego, inicia un bucle que itera sobre cada elemento de la lista pares, asignando el valor actual a la variable numero. En cada iteración, se imprime en la consola el resultado de multiplicar numero por 2.
+
+pares = [2,4,6,8,10]
+for numero in pares:
+    print(numero * 2)
